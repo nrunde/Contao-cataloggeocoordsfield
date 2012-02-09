56ce9baa764fc2f264d52e8d4e42ba13079e257e
Nikolas Runde <nikolas.runde@gmail.com>
2012-02-09 11:25:50 +0100

    Kommentar angepasst.
    Change-Id: Iedf980ec9819f1386604cebc269fd215ae8996b0

--- CatalogGeoCoordsField.php
+++ CatalogGeoCoordsField.php
@@ -20,7 +20,7 @@
  * @license    GPL 
  * @filesource
  */
-//Kommentar zum Testen
+// Kommentar zum Testen von Zend+Github
 // class to manipulate the field info to be as we want it to be, to render it and to make editing possible.
 class CatalogGeoCoordsField extends Backend {
 
